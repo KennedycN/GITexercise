7cd2acb9568e93abeae49af2bc2c8fe7335063b8
KennedycN <52343424+KennedycN@users.noreply.github.com>
2020-11-22 12:09:16 +0100

    Second file commit

---
+++ Platz.js
@@ -0,0 +1,55 @@
+//alert("Hello,Platz!");
+
+/*var a = 5;
+var b ="5";
+
+if (a===b)
+{
+	console.log("True")
+}else{console.log("False")}*/
+
+/*var player_one = 1
+var play_two = 2
+var highscore= (player_one > play_two) ? play_two:play_two;
+var a = 1;
+while (a<10)
+{
+	console.log(a+" Hey")
+	a++;
+}
+var a = 0;
+do{
+	console.log (a+ "Wie Ghetz");
+	a++;
+}while (a<10);
+
+for (a=1;a<=20; a++)
+{ console.log(a+" Ich bin gut");
+if (a == 6){console.log(" Hier ist 6")}}
+
+function myfunction(x,y,z)
+{ console.log(x+y+z)}
+function looper(x,y,z)
+{
+	for (a =x;a<y;a++)
+	{
+		console.log(a+ " Hey");
+		if (a==z)
+		{ 
+			console.log("Yes");
+			continue;
+		}
+	}
+}
+var someData = [10,20,50,30,60];
+ var newsomeData = someData.sort(); 
+ console.log(newsomeData);
+ var newData = someData.reverse(); 
+ console.log(newData);
+ var nData = someData.join(); 
+ console.log(nData);*/
+
+ var today = new Date()
+ console.log(today)
+ 
+ 
